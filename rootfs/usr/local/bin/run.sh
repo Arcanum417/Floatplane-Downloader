490fc2471d703e07c81e8c443bb498710e07205a
Rob Gökemeijer <rob@rob-bit.nl>
2018-07-12 01:17:30 +0200

    Docker changes

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -2,6 +2,11 @@
 
 echo "Starting"
 
+echo "Moving settings file if it doesn't exist..."
+if [ ! -f ${CONFIG_PATH}/default.json ]; then
+  mv /opt/Floatplane-Downloader/settings.json $CONFIG_PATH
+fi
+
 echo "Updating permissions..."
 for dir in /opt/Floatplane-Downloader /etc/s6.d /config /media; do
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
@@ -12,5 +17,10 @@ for dir in /opt/Floatplane-Downloader /etc/s6.d /config /media; do
   fi
 done
 echo "Done updating permissions."
+
+
+sed -i 's/"user": "!",/"user": "'$Username'",/' /opt/Floatplane-Downloader/settings.json
+sed -i 's/"password": "!",/"password": "'$Password'",/' /opt/Floatplane-Downloader/settings.json
+sed -i 's/"videoFolder": "!",/"videoFolder": "'$Media'",/' /opt/Floatplane-Downloader/settings.json
 
 su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
