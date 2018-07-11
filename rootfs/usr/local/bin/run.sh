e30865b2f66d9e433ed66a43c89cccea8fd27bb8
Rob Gökemeijer <rob@rob-bit.nl>
2018-07-12 01:38:57 +0200

    More Testing

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -3,12 +3,12 @@
 echo "Starting"
 
 echo "Moving settings file if it doesn't exist..."
-if [ ! -f ${CONFIG_PATH}/settings.json ]; then
-  mv /opt/Floatplane-Downloader/settings.json $CONFIG_PATH
+if [ ! -f /config/settings.json ]; then
+  mv /opt/Floatplane-Downloader/settings.json /config
 fi
 
 echo "Updating permissions..."
-for dir in /opt/Floatplane-Downloader /etc/s6.d /config /media; do
+for dir in /opt/Floatplane-Downloader /etc/s6.d /config /media/floatplane; do
   if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
     echo "Updating permissions in $dir..."
     chown -R $UID:$GID $dir
@@ -18,9 +18,13 @@ for dir in /opt/Floatplane-Downloader /etc/s6.d /config /media; do
 done
 echo "Done updating permissions."
 
-
+echo "Setting up settings"
+echo $USERNAME
+echo ${USERNAME}
 sed -i 's/"user": "",/"user": "'$USERNAME'",/' /opt/Floatplane-Downloader/settings.json
 sed -i 's/"password": "",/"password": "'$PASSWORD'",/' /opt/Floatplane-Downloader/settings.json
 sed -i 's/"videoFolder": "",/"videoFolder": "'$MEDIA_PATH'",/' /opt/Floatplane-Downloader/settings.json
+
+echo "Done setting up settings"
 
 su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
