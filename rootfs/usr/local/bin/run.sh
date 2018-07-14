19fc3287165b8654cf765451cbfb5d2a427df924
Rob Gökemeijer <grob1998@me.com>
2018-07-14 23:59:23 +0200

    Docker experiment

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -6,7 +6,14 @@ echo node version: `node --version`
 
 if [ "$JUST_RUN" = "N" ]; then
   git clone $GIT_URL /app
+  npm install
 fi
+
+echo "Setting up settings"
+sed -i 's/"user": "",/"user": "'$USERNAME'",/' /app/settings.json
+sed -i 's/"password": ""/"password": "'$PASSWORD'"/' /app/settings.json
+sed -i 's/"videoFolder": ".*",/"videoFolder": "${MEDIA_PATH//\//\\/}",/' /app/settings.json
+echo "Done setting up settings"
 
 echo "Moving settings file"
 if [ ! -f $CONFIG_PATH/settings.json ]; then
@@ -15,22 +22,5 @@ else
   cp -f $CONFIG_PATH/settings.json /app
 fi
 echo "Done moving settings file"
-
-echo "Updating permissions..."
-for dir in /app /etc/s6.d $CONFIG_PATH $MEDIA_PATH; do
-  if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
-    echo "Updating permissions in $dir..."
-    chown -R $UID:$GID $dir
-  else
-    echo "Permissions in $dir are correct."
-  fi
-done
-echo "Done updating permissions."
-
-echo "Setting up settings"
-sed -i 's/"user": "",/"user": "'$USERNAME'",/' /app/settings.json
-sed -i 's/"password": ""/"password": "'$PASSWORD'"/' /app/settings.json
-sed -i 's/"videoFolder": ".*",/"videoFolder": "'$MEDIA_PATH'",/' /app/settings.json
-echo "Done setting up settings"
 
 su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
