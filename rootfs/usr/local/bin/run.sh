89672fadd6f4e287f737317f7da7799aea55a32e
Rob Gökemeijer <grob1998@me.com>
2018-08-05 18:38:19 +0200

    Fix not updating

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -5,8 +5,14 @@ echo "Starting"
 echo node version: `node --version`
 
 if [ "$JUST_RUN" = "N" ]; then
+  echo "Getting newest version from GitHub"
+  cp /app/partial.json $CONFIG_PATH
+  rm -rf /app
   git clone $GIT_URL /app
   npm install
+  cp -f $CONFIG_PATH/settings.json /app
+  cp -f $CONFIG_PATH/partial.json /app
+  echo "Update complete"
 fi
 
 echo "Updating permissions..."
